b3d97d3afd2304e26103b8350238254cb58d5df7
Igor Shevchenko <cyberskunk@gmail.com>
2014-11-28 11:52:19 +0300

    posts editind, deleting

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,5 +3,8 @@ Template.postItem.helpers({
         var anchor = document.createElement('a');
         anchor.href = this.url;
         return anchor.hostname;
+    },
+    ownPost: function () {
+        return this.userId === Meteor.userId();
     }
 })
